f27b7ee8215a2f4f6130ea12234c7af85d1d095f
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2025-02-28 00:30:58 +0530

    Developed generic tabs

---
+++ Frontend/lib/tabs/explore_tab.dart
@@ -0,0 +1,13 @@
+// lib/screens/tabs/explore_tab.dart
+import 'package:flutter/material.dart';
+
+class ExploreTab extends StatelessWidget {
+  const ExploreTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Explore Tab Content'),
+    );
+  }
+}
---
+++ Frontend/lib/tabs/home_tab.dart
@@ -0,0 +1,13 @@
+// lib/screens/tabs/home_tab.dart
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home Tab Content'),
+    );
+  }
+}
