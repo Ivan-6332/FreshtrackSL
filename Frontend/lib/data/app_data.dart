2ea1e7d004fe3b6c6780a9ce49ff6b464ed8071d
nethmee03 <nethmeemudannayake@gmail.com>
2025-02-28 18:12:25 +0530

    Update app_data.dart
    Created the vegetables list in app_data

--- Frontend/lib/data/app_data.dart
+++ Frontend/lib/data/app_data.dart
@@ -1,3 +1,38 @@
 // lib/data/app_data.dart
+class AppData {
+  static final List<Map<String, dynamic>> vegetables = [
+    {
+      "id": 1,
+      "name": "Carrot",
+      "demand": 145.5,
+      "isFavorited": false,
+    },
+    {
+      "id": 2,
+      "name": "Tomato",
+      "demand": 178.2,
+      "isFavorited": true,
+    },
+    {
+      "id": 3,
+      "name": "Beans",
+      "demand": 92.8,
+      "isFavorited": true,
+    },
+    {
+      "id": 4,
+      "name": "Cabbage",
+      "demand": 115.3,
+      "isFavorited": false,
+    },
+    {
+      "id": 5,
+      "name": "Onion",
+      "demand": 167.9,
+      "isFavorited": true,
+    }
+  ];
 
-// class of AppData, with a static final lists for vegetables and categories will be coded here
+// categories list here
+
+}
