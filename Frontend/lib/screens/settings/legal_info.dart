0df6d4e73845102bf2c19289cbaef5d424db4264
Hathnagoda <gayanthahathnagoda79@gmail.com>
2025-03-01 12:21:05 +0530

    Developed the structure of legal_info

--- Frontend/lib/screens/settings/legal_info.dart
+++ Frontend/lib/screens/settings/legal_info.dart
@@ -1,1 +1,28 @@
 // lib/screens/settings/legal_info.dart
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LegalInfoScreen extends StatelessWidget {
+  const LegalInfoScreen({super.key});
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Legal Info'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          // add links here
+        ],
+      ),
+    );
+  }
+}
