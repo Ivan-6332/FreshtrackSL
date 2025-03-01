b84fa59bf1e712bb244bbcbb4e2be67db1bb81bd
Hathnagoda <gayanthahathnagoda79@gmail.com>
2025-03-01 12:16:42 +0530

    Developed the structure of account_settings

--- Frontend/lib/screens/settings/account_settings.dart
+++ Frontend/lib/screens/settings/account_settings.dart
@@ -1,1 +1,17 @@
 // lib/screens/settings/account_settings.dart
+import 'package:flutter/material.dart';
+
+class AccountSettingsScreen extends StatelessWidget {
+  const AccountSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Settings'),
+      ),
+      body: 
+      // code here ,
+    );
+  }
+}
