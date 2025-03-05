698e4535bfc4ba9d0fb5662f1b645d72102ac957
ParamaHerath <paramaherath@gmail.com>
2025-03-05 22:51:50 +0530

    Fixed the import statement for shared_preferences in language_provider.dart
    The import statement was trying to find a local file called shared_preferences.dart instead of using the package

--- Frontend/lib/providers/language_provider.dart
+++ Frontend/lib/providers/language_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider extends ChangeNotifier {
   static const String _languageKey = 'selected_language';
