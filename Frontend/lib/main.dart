75cf989e6190486062a2df7cfd4ae634b1703eb9
ParamaHerath <paramaherath@gmail.com>
2025-03-05 21:50:32 +0530

    Updated the main.dart file to include localization support

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,21 +1,13 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/main_screen.dart';
-import 'services/supabase_service.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'config/app_localizations.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
-
-  await SupabaseService().initialize(
-    'https://zdawprzqlmhmflbuajlo.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkYXdwcnpxbG1obWZsYnVhamxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NzQ5MDksImV4cCI6MjA1NjE1MDkwOX0.QhSazeIFBFfDpDBbA7QhO-87FBTn51KZQ1pcaD7lg_g',
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -38,6 +30,13 @@ class MyApp extends StatelessWidget {
         '/signup': (context) => const SignupScreen(),
         '/main': (context) => const MainScreen(),
       },
+      localizationsDelegates: const [
+        AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
