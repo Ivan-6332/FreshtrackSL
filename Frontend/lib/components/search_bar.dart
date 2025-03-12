f0d9e24f43f27aa771eabd04e93421e8e5be0879
Ivan-6332 <ivanakalanka@gmail.com>
2025-03-12 12:22:09 +0530

    styled search bar

--- Frontend/lib/components/search_bar.dart
+++ Frontend/lib/components/search_bar.dart
@@ -12,68 +12,101 @@ class SearchBarWithProfile extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(34), // Slightly larger to accommodate thick border
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.green.shade300,
+              Colors.green.shade500,
+              Colors.teal.shade400,
+            ],
+          ),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.08),
-              blurRadius: 10,
+              color: Colors.green.withOpacity(0.3),
+              blurRadius: 12,
               offset: const Offset(0, 4),
             ),
           ],
         ),
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: localizations.get('search'),
-            hintStyle: TextStyle(
-              color: Colors.grey.shade500,
-              fontWeight: FontWeight.w400,
-              fontSize: 16,
-            ),
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Icon(
-                Icons.search_rounded,
-                color: Colors.black45,
-                size: 24,
-              ),
-            ),
-            suffixIcon: Container(
-              margin: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.white12,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Icon(
-                Icons.tune_rounded,
-                color: Colors.black45,
-                size: 25,
-              ),
-            ),
-            filled: true,
-            fillColor: Colors.white,
-            border: OutlineInputBorder(
+        child: Padding(
+          padding: const EdgeInsets.all(3.5), // Thicker border
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(30),
-              borderSide: BorderSide.none,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.03),
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: BorderSide(
-                color: Colors.black,
-                width: 1.5,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: localizations.get('search'),
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade500,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Icon(
+                    Icons.search_rounded,
+                    color: Colors.black45,
+                    size: 24,
+                  ),
+                ),
+                suffixIcon: Container(
+                  margin: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.green.shade400,
+                        Colors.teal.shade400,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.green.withOpacity(0.2),
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(
+                    Icons.tune_rounded,
+                    color: Colors.white,
+                    size: 22,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              ),
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
             ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: BorderSide(
-                color: Colors.grey.shade200,
-                width: 1.0,
-              ),
-            ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-          ),
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
           ),
         ),
       ),
